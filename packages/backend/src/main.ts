6a4d2f80a9e0854cf8ca13b90a2a675e82711ce6
alextsx <c0ltwinters@protonmail.com>
2024-01-26 21:33:33 +0100

    fix: add missing cookie-parser middleware

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,13 +1,15 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get<ConfigService>(ConfigService);
-
   const port = configService.get('app.port');
+
+  app.use(cookieParser());
 
   await app.listen(port);
 }
